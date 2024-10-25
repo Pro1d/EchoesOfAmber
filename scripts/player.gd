e1d05fe8864b92f6054db35300aca341bd39241e
josue.alvarez <josue.alvarez@hotmail.fr>
2024-10-25 20:19:22 +0200

    Put leaves

--- scripts/player.gd
+++ scripts/player.gd
@@ -15,13 +15,28 @@ func _ready() -> void:
 	leaves_attraction_area.connect("body_exited", _on_leave_exited_area)
 
 func _physics_process(_delta: float) -> void:
+	
 	var command := Vector2(
 		Input.get_axis("move_left", "move_right"),
 		Input.get_axis("move_up", "move_down"),
 	)
+	
+	var attract_leaves := Input.is_action_pressed('attract_leaves')
+	
+	if not attract_leaves: # dont move while attracting leaves
+		velocity = velocity.move_toward(command.normalized() * SPEED, ACCEL)
 
-	velocity = velocity.move_toward(command.normalized() * SPEED, ACCEL)
-	leaves_attraction_area.monitoring = Input.is_action_pressed('attract_leaves')
+	leaves_attraction_area.monitoring = attract_leaves
+	
+	if Input.is_action_just_pressed("put_red"):
+		pass
+	
+	if Input.is_action_just_pressed("put_green"):
+		pass
+		
+	if Input.is_action_just_pressed("put_yellow"):
+		pass
+
 	move_and_slide()
 	
 func _on_leave_entered_area(body: Leave) -> void:
