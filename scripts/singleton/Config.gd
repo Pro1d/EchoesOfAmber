b98f3484675bffd4fed60a422b491722358d55b3
Pro1d <topAffaire_pg@hotmail.fr>
2024-10-25 09:45:20 +0200

    Config singleton

--- scripts/singleton/Config.gd
+++ scripts/singleton/Config.gd
@@ -1,17 +1,9 @@
 extends Node
 
-#const CursorArrowIcon := preload("res://assets/images/ui/cursor_arrow2.atlastex")
-#const CursorIBeamIcon := preload("res://assets/images/ui/cursor_ibeam.atlastex")
-#const CursorAimIcon := preload("res://assets/images/ui/cursor_aim1.atlastex")
+#const CursorArrowIcon := preload("res://assets/resources/ui/cursor_arrow.atlastex")
 
 func _enter_tree() -> void:
 #	Input.set_custom_mouse_cursor(
 #		CursorArrowIcon, Input.CURSOR_ARROW, Vector2(2, 2)
-#	)
-#	Input.set_custom_mouse_cursor(
-#		CursorIBeamIcon, Input.CURSOR_IBEAM, Vector2(16, 16)
-#	)
-#	Input.set_custom_mouse_cursor(
-#		CursorAimIcon, Input.CURSOR_CROSS, Vector2(16, 16)
 #	)
 	pass
