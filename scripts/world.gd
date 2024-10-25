b3369c26cadb4e0500d955a80a45736be808c7cc
josue.alvarez <josue.alvarez@hotmail.fr>
2024-10-25 19:58:51 +0200

    HUD connect

--- scripts/world.gd
+++ scripts/world.gd
@@ -2,6 +2,7 @@ extends Node2D
 class_name World
 
 @onready var player : Player = %Player
+@onready var hud : HUD = %HUD
 
 var leaves_count := {
 	Leave.LeaveType.RED: 0,
@@ -14,3 +15,8 @@ func _ready() -> void:
 
 func _on_leave_in_backpack(leave_type: Leave.LeaveType) -> void:
 	leaves_count[leave_type] += 1
+	
+	hud.update_leave_count([
+		leaves_count[Leave.LeaveType.RED],
+		leaves_count[Leave.LeaveType.YELLOW],
+		leaves_count[Leave.LeaveType.GREEN]], true)
