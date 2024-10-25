89b56705c07c5a9e138f4c3b42e04558a3f6233e
Pro1d <topAffaire_pg@hotmail.fr>
2024-10-25 19:22:22 +0200

    fix hud script

--- scripts/ui/hud.gd
+++ scripts/ui/hud.gd
@@ -7,20 +7,22 @@ extends Control
 	%Leave1Label, %Leave2Label, %Leave3Label
 ]
 var _leave_tweens : Array[Tween] = [null, null, null]
-var _displayed_leaves : Array[int] = [0, 0, 0]
+var _displayed_leave_counts : Array[int] = [0, 0, 0]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	update_leave_count(_displayed_leaves, false)
+	update_leave_count(_displayed_leave_counts, false)
 
 func update_leave_count(leaves: Array[int], animate: bool) -> void:
 	for i in range(_leave_labels.size()):
-		_leave_labels[i].text = str(leaves[i])
+		_leave_labels[i].text = str(leaves[i] / 10)
 		
 		# Animation
-		if _displayed_leaves[i] != leaves[i] and animate:
+		if _displayed_leave_counts[i] != leaves[i] and animate:
+			_displayed_leave_counts[i] = leaves[i]
 			if _leave_tweens[i] != null:
 				_leave_tweens[i].kill()
 			_leave_tweens[i] = create_tween()
 			_leave_tweens[i].tween_property(_leave_textures[i], "position:y", 0, 0.5) \
 				.from(-12.0).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
+			
