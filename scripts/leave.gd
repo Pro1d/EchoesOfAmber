3fd1edf5a80b86899d3c60ad9dd2973e1d243a65
Pro1d <topAffaire_pg@hotmail.fr>
2024-10-25 13:38:48 +0200

    remove "self." to enable linting

--- scripts/leave.gd
+++ scripts/leave.gd
@@ -1,7 +1,7 @@
 extends RigidBody2D
 class_name Leave
 
-var atraction_point: Node2D = null
+var attraction_point: Node2D = null
 
 func _ready() -> void:
 	pass 
@@ -12,13 +12,13 @@ func _process(_delta: float) -> void:
 	
 	
 func _physics_process(_delta: float) -> void:
-	if self.atraction_point != null:
+	if attraction_point != null:
 		# TODO : change the vector in function of the distance
-		var target := atraction_point.global_position
-		var pull_vector := (target - self.global_position).normalized()
+		var target := attraction_point.global_position
+		var pull_vector := (target - global_position).normalized()
 		var tangent := Vector2(-pull_vector.y, pull_vector.x)
 		var max_dist := 96.0
-		var distance_ratio := clampf((target - self.global_position).length() / max_dist, 0.0, 1.0)
+		var distance_ratio := clampf((target - global_position).length() / max_dist, 0.0, 1.0)
 		
 		var pull_force := 300.0 * (1.0 - distance_ratio)
 		var tangential_force := 150.0 * distance_ratio
@@ -29,4 +29,4 @@ func _physics_process(_delta: float) -> void:
 	
 
 func set_attraction_point(obj: Node2D) -> void:
-	self.atraction_point = obj
+	attraction_point = obj
