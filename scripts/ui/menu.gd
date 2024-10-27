4b8701842a870cf4d44b2e428d554fac47334446
josue.alvarez <josue.alvarez@hotmail.fr>
2024-10-27 10:01:56 +0100

    Sounds for writting in menu

--- scripts/ui/menu.gd
+++ scripts/ui/menu.gd
@@ -5,6 +5,7 @@ signal play_clicked
 
 @onready var _play_button := %PlayButton as Button
 @onready var _quest_label : RichTextLabel = %QuestLabel
+@onready var _writing_sfx : AudioStreamPlayer = %WritingFX
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,9 +14,10 @@ func _ready() -> void:
 func display_current_quest_text(txt: String) -> void:
 	_quest_label.append_text(txt)
 	_quest_label.visible_characters = 0
+	_writing_sfx.play()
 	
 	for i in range(0, len(txt), 1):
 		_quest_label.visible_characters = i
 
-		for _f in range(3):
+		for _f in range(2):
 			await get_tree().process_frame
