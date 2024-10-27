0ca9d1d06ccc54e2589c1bec53f4f328c612ad8a
Pro1d <topAffaire_pg@hotmail.fr>
2024-10-27 17:18:56 +0100

    export config

--- build/export.sh
+++ build/export.sh
@@ -2,12 +2,13 @@
 #
 # Install butler: https://itch.io/docs/butler/installing.html
 
-itch_project=test
+itch_project=echoes-of-amber
 itch_user=proyd
 itch_upload=1
 
 
 # WEB
+mkdir -p web
 godot4 --quiet --headless --export-release Web build/web/index.html ../project.godot
 rm $itch_project"_"web.zip 2> /dev/null
 zip -j $itch_project"_"web.zip web/* > /dev/null
@@ -17,6 +18,7 @@ if [[ $itch_upload = 1 ]]; then
 fi
 
 # WINDOWS
+mkdir -p windows
 godot4 --quiet --headless --export-release Windows build/windows/$itch_project.exe ../project.godot
 rm $itch_project"_"windows.zip 2> /dev/null
 zip -j $itch_project"_"windows.zip windows/* > /dev/null
@@ -26,6 +28,7 @@ if [[ $itch_upload = 1 ]]; then
 fi
 
 # LINUX
+mkdir -p linux
 godot4 --quiet --headless --export-release Linux build/linux/$itch_project.x86_64 ../project.godot
 rm $itch_project"_"linux.zip 2> /dev/null
 zip -j $itch_project"_"linux.zip linux/* > /dev/null
