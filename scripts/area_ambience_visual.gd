eb48cbffc0a84e0c5fcd1995a2bbdf5c4f792daa
Pro1d <topAffaire_pg@hotmail.fr>
2024-10-27 12:28:21 +0100

    god rays and fix ground under house

--- scripts/area_ambience_visual.gd
+++ scripts/area_ambience_visual.gd
@@ -7,13 +7,17 @@ class_name AreaAmbienceVisual
 # True if this audio corresponds to a desolated area.
 @export var is_desolated: bool = false
 
-@export var items : Array[CanvasItem] = []
+var items : Array[CanvasItem] = []
 @export var fade_duration: float = 1
+
+func _ready() -> void:
+	items.append_array(get_children())
 
 func transition(animate: bool, shown: bool) -> void:
 	if animate:
 		var tween := get_tree().create_tween()
 
+		var ease_type := Tween.EASE_OUT if shown else Tween.EASE_IN
 		var final_val := Color(1, 1, 1, 1) if shown else Color(1, 1, 1, 0)
 		var start_val := Color(1, 1, 1, 1) if not shown else Color(1, 1, 1, 0)
 		for item in items:
@@ -21,8 +25,9 @@ func transition(animate: bool, shown: bool) -> void:
 				item.visible = true
 				item.modulate = start_val
 
-			tween.parallel().tween_property(item, 'modulate', final_val, fade_duration)
-	
+			tween.parallel().tween_property(item, 'modulate', final_val, fade_duration). \
+				set_ease(ease_type).set_trans(Tween.TRANS_QUAD)
+			
 		await tween.finished
 		
 		tween.kill()
