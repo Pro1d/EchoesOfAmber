5465e398c8584cf3fcf5a3eccc3fcf8e8fc6f103
josue.alvarez <josue.alvarez@hotmail.fr>
2024-10-27 15:41:55 +0100

    Tweak volumes

--- scripts/fx/global_sfx.gd
+++ scripts/fx/global_sfx.gd
@@ -12,7 +12,28 @@ func _enter_tree() -> void:
 	Config.sfx = self
 
 func play_area_cleared() -> void:
+	var music_bus_id := AudioServer.get_bus_index("Music")
+	var current_volume := AudioServer.get_bus_volume_db(music_bus_id)
+	
+	# Lower music volume when playing this SFX
+	var tween := get_tree().create_tween()
+	tween.tween_method(_set_music_bus_volume, current_volume, current_volume - 16.0, 0.5)
+	await tween.finished
+	tween.kill()
+	
 	area_cleared_sfx.play()
+	
+	await area_cleared_sfx.finished
+	
+	# Put back original volume
+	tween = get_tree().create_tween()
+	tween.tween_method(_set_music_bus_volume, AudioServer.get_bus_volume_db(music_bus_id), current_volume, 2)
+	await tween.finished
+	tween.kill()
+
+func _set_music_bus_volume(volume: float) -> void:
+	var music_bus_id := AudioServer.get_bus_index("Music")
+	AudioServer.set_bus_volume_db(music_bus_id, volume)
 
 func play_build_fx() -> void:
 	build_sfx.pitch_scale = randf_range(0.9, 1.1)
@@ -26,7 +47,7 @@ func play_page_turn_fx() -> void:
 	page_turn_sfx.play()
 
 func play_music_layer_1() -> void:
-	fadein_track(music_layer_1, -2.0)
+	fadein_track(music_layer_1, -6.0)
 
 func play_music_layer_2() -> void:
 	fadein_track(music_layer_2, -17.0)
