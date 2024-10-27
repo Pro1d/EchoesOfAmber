5af5828a3826ba7c6802bfadd1bbf26c68037a5c
josue.alvarez <josue.alvarez@hotmail.fr>
2024-10-27 17:54:10 +0100

    Add code for crows and birds

--- scripts/bird.gd
+++ scripts/bird.gd
@@ -27,9 +27,10 @@ class Target:
 
 
 # Parameters
-var anim_duration := 1.5
-var cycle_grounded_duration : float = 5
-var cycle_away_duration : float = 1
+@export_range(1, 3) var anim_duration := 1.5
+@export_range(0.5, 10) var cycle_grounded_duration : float = 2.5
+@export_range(0.5, 10) var cycle_away_duration : float = 1
+@export_range(0.0, 1,0) var cycle_random_percentage : float = 0.8
 
 var high_position_px := -24.0
 var x_amplitude := 256.0
@@ -72,12 +73,24 @@ func _process(delta: float) -> void:
 
 	prev_state = state
 
-func transition(_a: bool, _s: bool) -> void:
-	pass
+func transition(animate: bool, shown: bool) -> void:
+	if animate:
+		if shown:
+			state = State.FlyinOutCyle
+			sub_state = SubState.Away
+		else:
+			state = State.FlyingOut
+	else:
+		sprite.visible = shown
+		if shown:
+			state = State.FlyinOutCyle
+			sub_state = SubState.Grounded
+		else:
+			state = State.BeingIdle
 
 # Starts the fly in / fly out cycle
 func start_cycle() -> void:
-	time_since_last_change = 0.0
+	time_since_last_change = randf_range(0, 4.5)
 	pass
 
 func update_cycle(delta: float) -> void:
@@ -87,31 +100,29 @@ func update_cycle(delta: float) -> void:
 		return
 	
 	if sub_state == SubState.Grounded and time_since_last_change >= cycle_grounded_duration:
-		fly_away()
-		time_since_last_change = 0.0
+		await fly_away()
+		time_since_last_change = randf_range(-cycle_away_duration * cycle_random_percentage, cycle_away_duration * cycle_random_percentage)
 	elif sub_state == SubState.Away and time_since_last_change >= cycle_away_duration:
-		fly_in()
-		time_since_last_change = 0.0
+		await fly_in()
+		time_since_last_change = randf_range(-cycle_grounded_duration * cycle_random_percentage, cycle_grounded_duration * cycle_random_percentage)
+		
 		
 
 func be_idle() -> void:
-	if sub_state != SubState.Grounded:
-		await fly_in()
+	pass
 
 
 func fly_in() -> void:
 	sub_state = SubState.Transitioning
 	visible = true
-	anim.play('Flying')
+	anim.play('Fly')
 	
 	var target : Target = target_positions.pick_random()
 	var start_pos := target.pos
 	var arrival_offset := Vector2(0, high_position_px if target.is_high else 0.0)
 	var start_y : float = min(start_pos.y - y_amplitude, get_viewport().get_camera_2d().global_position.y - y_amplitude)
 	global_position = Vector2(start_pos.x + randf_range(-x_amplitude, 0), start_y)
-	
-	print("flying to high = ", target.is_high)
-	
+
 	var tween := get_tree().create_tween()
 	tween.tween_property(self, 'global_position', start_pos, anim_duration)
 	tween.tween_property(sprite, 'position', arrival_offset, 0.5)
@@ -122,7 +133,7 @@ func fly_in() -> void:
 
 func fly_away() -> void:
 	sub_state = SubState.Transitioning
-	anim.play('Flying')
+	anim.play('Fly')
 	var end_y : float = min(global_position.y - y_amplitude, get_viewport().get_camera_2d().global_position.y - y_amplitude)
 	var end_x := global_position.x + randf_range(0, x_amplitude)
 
